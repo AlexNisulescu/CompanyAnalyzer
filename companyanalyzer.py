4d8e469d51d81bf39f3bb0b438de7613dd1ec2e5
anisulescu <alexandru.nisulescu@gmail.com>
2022-10-16 00:03:24 +0300

    Created the intrinsic value
    I have added some functions that are used to calculate the intrinsic
    value of a company using Benjamin Graham formula
    
    Signed-off-by: anisulescu <alexandru.nisulescu@gmail.com>

--- companyanalyzer.py
+++ companyanalyzer.py
@@ -1,6 +1,32 @@
+from multiprocessing import current_process
 import yfinance as yf # Used for financial data
 import pandas as pd # Used to store data in a dataframe
 from datetime import date
+
+
+def earning_per_share_calculator(price_to_earnings, current_price):
+    return current_price / price_to_earnings
+
+def growth_rate_calculator(history_price, current_price):
+    return ((history_price + current_price) / history_price)
+
+# This function is used to calculate the intrinsic value of a company
+def intrinsic_value_calculator(price_to_earnings, current_price, ten_years_price):
+    # V = [ EPS * (8.5 + 2 * g) * 4.4 ] / Y , where:
+    # EPS = earnings_per_share
+    # g = growth_rate
+    # Y = bond_yield
+    Y = 5.21
+    earnings_per_share = earning_per_share_calculator(price_to_earnings, current_price)
+    growth_rate = growth_rate_calculator(ten_years_price, current_price)
+    return int((earnings_per_share * (8.5 + (2 * growth_rate)) * 4.4) / Y)
+
+# This function is used to calculate the margin of safety of a company
+def margin_of_safety(intrinsic_value, current_market_price):
+    # MS = ( V - CMP ) / V , where:
+    # V = intrinsic_value
+    # CMP = current_market_price
+    return (intrinsic_value - current_market_price) / intrinsic_value
 
 # This function is used to read the list of tickers
 def read_tickers(filename):
@@ -11,9 +37,9 @@ def read_tickers(filename):
 # This function creates the dataframe structure that will store the data
 def create_df():
     df = pd.DataFrame(columns=['Name', 'Sector', 'Industry', 'MarketCap',
-                        'P/E', 'Current Price', 'Return on Equity', 
-                        'Return on Assets', 'Current Ratio', 'Debt to Equity',
-                        'Profit Margins', 'Gross Margins', 
+                        'P/E', 'Current Price', 'Intrinsic Value',
+                        'Return on Equity', 'Return on Assets', 'Current Ratio',
+                        'Debt to Equity', 'Profit Margins', 'Gross Margins', 
                         'Five Years Average Dividend Yeald', 'Last Dividend Value',
                         'Three Year Average Return', 'Five Year Average Return', 
                         'Worth', 'Total Cash', 'Total Assets', 'Total Debt', 
@@ -44,19 +70,25 @@ def get_details(tkrs):
         except TypeError:
             details.info['totalDebt'] = 0
         worth = calculate_worth(details.info['totalCash'], details.info['totalAssets'], details.info['totalDebt'])
+        ten_years_price = details.history(period='10y')
+        current_price = details.history(period='1d')
+        intrinsic_value = intrinsic_value_calculator(details.info['trailingPE'],
+                          current_price['Close'][0], ten_years_price['Close'][0])
         # Creates a new row for the dataframe
         new_row = {'Name':ticker, 'Sector':details.info['sector'], 'Industry':
         details.info['industry'], 'MarketCap':details.info['marketCap'],
         'P/E':details.info['trailingPE'], 'Current Price':
-        details.info['currentPrice'], 'Debt to Equity':details.info['debtToEquity'],
+        details.info['currentPrice'], 'Intrinsic Value':intrinsic_value,
+        'Debt to Equity':details.info['debtToEquity'],
         'Profit Margins':details.info['profitMargins'], 'Gross Margins':
         details.info['grossMargins'], 'Five Years Average Dividend Yeald':
         details.info['fiveYearAvgDividendYield'], 'Last Dividend Value':
         details.info['lastDividendValue'], 'Three Year Average Return':
         details.info['threeYearAverageReturn'], 'Five Year Average Return':
-        details.info['fiveYearAverageReturn'], 'Worth':worth ,'Total Cash':details.info['totalCash'],
-        'Total Assets':details.info['totalAssets'], 'Total Debt':
-        details.info['totalDebt'], 'Forward P/E':details.info['forwardPE']}
+        details.info['fiveYearAverageReturn'], 'Worth':worth ,
+        'Total Cash':details.info['totalCash'],'Total Assets':
+        details.info['totalAssets'], 'Total Debt':details.info['totalDebt'],
+        'Forward P/E':details.info['forwardPE']}
         # Appends the new row to the dataframe
         df = df.append(new_row, ignore_index=True)
     # Sorts the companies by MarketCap
